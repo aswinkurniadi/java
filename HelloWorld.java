212bc862d2a265346a00bad352af6fa6c2f65d4d
aswinkurniadi <aswinkurniadi2@gmail.com>
2020-04-19 05:47:20 +0700

    initialisasi

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+//Program menampilkan hello world
+public class HelloWorld
+{
+  public static void main(String[] args)
+  {
+    System.out.print("Hello World");
+  }
+}
---
+++ Perkenalan.java
@@ -0,0 +1,9 @@
+public class Perkenalan
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Assalamualaikum");
+    System.out.println("Perkenalkan, nama saya Aswin Kurniadi.");
+    System.out.println("Saya sedang belajar Java.");
+  }
+}
