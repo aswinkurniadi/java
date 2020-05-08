9f5b2f46af1ae83f8bde91dc505aa9821afc5247
aswinkurniadi <aswinkurniadi2@gmail.com>
2020-05-08 22:26:05 +0700

    Menambahkan Kasus Operator Ternary

---
+++ Percabangan/OperatorTernary.java
@@ -0,0 +1,14 @@
+public class OperatorTernary {
+    public static void main(String[] args) {
+
+        int score = 80;
+        String hasil;
+
+        // menggunakan operator ternary
+        hasil = score > 70 ? "Lulus" : "Tidak Lulus";
+
+        // menampilkan hasil
+        System.out.println(hasil);
+
+    }
+}
